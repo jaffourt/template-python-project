44fd709ddee582863111e0246cbe96f445c11e15
josefaffourtit <josef.affourtit@gmail.com>
2023-01-03 14:06:59 -0500

    Add tests to linter check

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,11 +1,14 @@
+"""A simple unit test for the project's core functionality"""
 import unittest
 
 from my_package.core import make_request
 
 
 class TestMakeRequest(unittest.TestCase):
+    """Test cases for make_request method"""
 
     def test_make_request(self):
+        """Test default and custom behavior"""
         # Test default behavior
         default = make_request()
         self.assertEqual(default.status_code, 200)
